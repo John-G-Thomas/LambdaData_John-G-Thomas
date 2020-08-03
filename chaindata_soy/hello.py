342ebf2aa617a72d7ded2931c4e050061fbcd225
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-03 16:54:28 -0400

    fixed setup

--- chaindata_soy/hello.py
+++ chaindata_soy/hello.py
@@ -1,6 +1,6 @@
-class chaindata_soy
-    def chaindata_date(self):
-        self['Year'] = df['date_column'].dt.to_period('Y')
-        self['Month'] = df['date_column'].dt.to_period('M')
-        self['Day'] = df['date_column'].dt.to_period('D')
-         return self
+#class chaindata_soy
+    #def chaindata_date(self):
+        #self['Year'] = df['date_column'].dt.to_period('Y')
+        #self['Month'] = df['date_column'].dt.to_period('M')
+       #self['Day'] = df['date_column'].dt.to_period('D')
+        # return self
