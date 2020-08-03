97633930d1bcba5118c6a0662f8445e11dc6c617
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-03 19:03:09 -0400

    fixed packages

--- chaindata_soy/__init__.py
+++ chaindata_soy/__init__.py
@@ -4,6 +4,6 @@ chaindata - a collection of Data Science helper functions
 
 import numpy as np
 import pandas as pd
-from chaindata_soy.dataframe_helpers import chaindata_date
+from chaindata_soy.dataframe_helper import chaindata_dates
 TEST = pd.DataFrame(np.ones(10))
 
--- chaindata_soy/dataframe_helper.py
+++ chaindata_soy/dataframe_helper.py
@@ -1,9 +1,15 @@
 """
 Function to add date columns from datetime column
 """
+import pandas as pd
+import datetime as dt
 
 
-def chaindata_date(df: Object) -> Object:
+def chaindata_dates(df: dt) -> dt:
+    """
+    :type df: datetime as dt
+    """
+    df['date_column'] = pd.to_datetime(df['date_column'])
     df['Year'] = df['date_column'].dt.to_period('Y')
     df['Month'] = df['date_column'].dt.to_period('M')
     df['Day'] = df['date_column'].dt.to_period('D')
