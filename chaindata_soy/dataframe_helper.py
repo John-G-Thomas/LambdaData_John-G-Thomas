15eab8f0c997c5009f60d940d2c23c7773c065f5
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-03 19:37:50 -0400

    fixed packages

--- chaindata_soy/dataframe_helper.py
+++ chaindata_soy/dataframe_helper.py
@@ -1,9 +1,17 @@
 """
-Function get date into int
+Function cleans dataframe
 """
 
 
-def get_date_int(df, date_column):
-    df["year"] = df[date_column].dt.year
-    df["month"] = df[date_column].dt.month
-    return year, month, week
+def clean(df):
+    df = df.copy()
+    # Drop NaN and extra columns and index
+    df.dropna(subset=[column], inplace=True)
+    df.set_index(pd.to_datetime(df['Datetime']), inplace=True)
+    df.drop('Datetime', axis=1, inplace=True)
+    return df
+
+
+"""
+clean datas and add pecific columns# def cleandate(df):
+"""
