4c5048de80bbfc5493be08ccf1a2a06328888052
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-04 11:42:45 -0400

    fixed packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="johnthomasg0215",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="John Thomas",
     author_email="johnthomasg0215@example.com",
     description="A small example package",
