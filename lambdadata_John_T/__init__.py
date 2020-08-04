20ebe3a0c9a1a8591d4316e32176c6857533fac0
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-04 15:53:56 -0400

    fixed

--- lambdadata_John_T/__init__.py
+++ lambdadata_John_T/__init__.py
@@ -4,6 +4,6 @@ Lambdadata_John-G-Thomas - a collection of Data Science helper functions
 import numpy as np
 import pandas as pd
 
-from dataframe_helper import ce
+from lambdadata.dataframe_helper import Asses
 
 TEST = pd.DataFrame(np.ones(10))
