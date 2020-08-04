689d57dbc44a3b69b4cdd2e8bbeed3fccf21c996
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-04 13:26:21 -0400

    added stuff

---
+++ chaindata_johnt/__init__.py
@@ -0,0 +1,10 @@
+"""
+chaindata - a collection of Data Science helper functions
+"""
+import numpy as np
+import pandas as pd
+
+from .dataframe_helper import clean, date
+from .file import Complex
+
+TEST = pd.DataFrame(np.ones(10))
