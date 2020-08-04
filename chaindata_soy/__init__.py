779750844403a3661c068e0d9628b7ffde4f9caf
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-03 22:48:18 -0400

    fixed packages

--- chaindata_soy/__init__.py
+++ chaindata_soy/__init__.py
@@ -4,5 +4,5 @@ chaindata - a collection of Data Science helper functions
 
 import numpy as np
 import pandas as pd
-from .dataframe_helper import "*"
+from .dataframe_helper import ~
 TEST = pd.DataFrame(np.ones(10))
