361095f7b9a551acc6bb421d2406f7fac1fe87b7
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-04 17:35:00 -0400

    fixed style and fixed function

--- lambdadata_John_T/dataframe_helper.py
+++ lambdadata_John_T/dataframe_helper.py
@@ -82,7 +82,7 @@ def plot_confusion_matrix(cm,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.show()
+    return plt.show()
 
 
 def assess_NA(data):
