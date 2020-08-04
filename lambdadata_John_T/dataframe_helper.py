0b1c9a40301ae249a17242295d5b1a501574e4fe
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-04 16:51:45 -0400

    fixed style

--- lambdadata_John_T/dataframe_helper.py
+++ lambdadata_John_T/dataframe_helper.py
@@ -12,16 +12,17 @@ def cleandates(df):
     df['Year'] = (df.select_dtypes(include=['datetime64']).to_period('Y'))
     df['Month'] = (df.select_dtypes(include=['datetime64']).to_period('M'))
     df['Day'] = (df.select_dtypes(include=['datetime64']).to_period('D'))
-    df.set_index(pd.to_datetime(df.select_dtypes(include=['datetime64'])), inplace=True)
+    df.set_index(pd.to_datetime(df.select_dtypes(include=['datetime64']
+                                                 )), inplace=True)
     df_cl = df.drop(df.select_dtypes(include=['datetime64']))
     return df_cl
 
 
 def assess_NA(data):
     """
-    Returns a pandas dataframe denoting the total number of NA values and the percentage of NA values in each column.
+    Returns a pandas dataframe denoting the total number of NA values and
+    the percentage of NA values in each column.
     The column names are noted on the index.
-
     Parameters
     ----------
     data: dataframe
@@ -29,12 +30,12 @@ def assess_NA(data):
     # pandas series denoting features and the sum of their null values
     null_sum = data.isnull().sum()  # instantiate columns for missing data
     total = null_sum.sort_values(ascending=False)
-    percent = (((null_sum / len(data.index)) * 100).round(2)).sort_values(ascending=False)
-
+    percent = (((null_sum / len(data.index)) * 100).round(2)).\
+        sort_values(ascending=False)
     # concatenate along the columns to create the complete dataframe
-    df_NA = pd.concat([total, percent], axis=1, keys=['Number of NA', 'Percent NA'])
-
-    # drop rows that don't have any missing data; omit if you want to keep all rows
+    df_NA = pd.concat([total, percent], axis=1,
+                      keys=['Number of NA', 'Percent NA'])
+    # drop rows that don't have any missing data; omit if you want to
+    # keep all rows
     df_NA = df_NA[(df_NA.T != 0).any()]
-
     return df_NA
