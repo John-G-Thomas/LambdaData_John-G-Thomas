a55ca787e7b5170ad1471f921261371a5f7d2da6
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-04 14:19:29 -0400

    added stuff

---
+++ lambdadata_John_T/__init__.py
@@ -0,0 +1,10 @@
+"""
+Lambdadata_John-G-Thomas - a collection of Data Science helper functions
+"""
+import numpy as np
+import pandas as pd
+
+# noinspection PyUnresolvedReferences
+from .dataframe_helper import cleaned, cleandate
+
+TEST = pd.DataFrame(np.ones(10))
