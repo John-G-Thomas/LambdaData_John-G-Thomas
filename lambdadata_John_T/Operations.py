563f472e65457a8c82d3df422acf267df2b8e43c
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-05 17:20:14 -0400

    Added Operations class which is a calculator function

--- lambdadata_John_T/Operations.py
+++ lambdadata_John_T/Operations.py
@@ -1,22 +1,24 @@
 """Python program to perform Addition Subtraction Multiplication
 and Division of two numbers"""
 
-num1 = int(input("Enter First Number: "))
-num2 = int(input("Enter Second Number: "))
 
-print("Enter which operation would you like to perform?")
-ch = input("Enter any of these char for specific operation +,-,*,/: ")
+class Operations:
+    num1 = int(input("Enter First Number: "))
+    num2 = int(input("Enter Second Number: "))
 
-result = 0
-if ch == '+':
-    result = num1 + num2
-elif ch == '-':
-    result = num1 - num2
-elif ch == '*':
-    result = num1 * num2
-elif ch == '/':
-    result = num1 / num2
-else:
-    print("Input character is not recognized!")
+    print("Enter which operation would you like to perform?")
+    ch = input("Enter any of these char for specific operation +,-,*,/: ")
 
-print(num1, ch, num2, ":", result)
+    result = 0
+    if ch == '+':
+        result = num1 + num2
+    elif ch == '-':
+        result = num1 - num2
+    elif ch == '*':
+        result = num1 * num2
+    elif ch == '/':
+        result = num1 / num2
+    else:
+        print("Input character is not recognized!")
+
+    print(num1, ch, num2, ":", result)
