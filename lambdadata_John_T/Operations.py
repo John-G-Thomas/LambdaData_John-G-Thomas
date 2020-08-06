11c266bbe80c9d59e3209d3f5a309b3c322827dd
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-06 16:11:48 -0400

    lecture notes

--- lambdadata_John_T/Operations.py
+++ lambdadata_John_T/Operations.py
@@ -2,28 +2,27 @@
 and Division of two numbers"""
 
 
-def Operation(num1, num2, ch):
-    if ch == '+':
-        result = num1 + num2
-        return print(num1, ch, num2, ":", result)
-    elif ch == '-':
-        result = num1 - num2
-        return print(num1, ch, num2, ":", result)
-    elif ch == '*':
-        result = num1 * num2
-        return print(num1, ch, num2, ":", result)
-    elif ch == '/':
-        result = num1 / num2
-        return print(num1, ch, num2, ":", result)
-    else:
-        return print("Input character is not recognized!")
-
-
-class math:
+class calculator:
     def __init__(self):
-        self.num1 = int(input("Enter First Number: "))
-        self.num2 = int(input("Enter Second Number: "))
         self.ch = input("Enter any of these char for specific operation +,-,*,/: ")
+        self.num2 = int(input("Enter Second Number: "))
+        self.num1 = int(input("Enter First Number: "))
+
+    def Operation(self, num1, num2, ch):
+        if self.ch == '+':
+            result = self.num1 + self.num2
+            return print(num1, ch, num2, ":", result)
+        elif self.ch == '-':
+            result = self.num1 - self.num2
+            return print(num1, ch, num2, ":", result)
+        elif self.ch == '*':
+            result = self.num1 * self.num2
+            return print(num1, ch, num2, ":", result)
+        elif self.ch == '/':
+            result = self.num1 / self.num2
+            return print(num1, ch, num2, ":", result)
+        else:
+            return print("Input character is not recognized!")
 
 
 """
