cb468f4b18d45a4dbbdaedbe64e4677c8b82135e
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-06 17:15:27 -0400

    lecture notes

--- lambdadata_John_T/Operations.py
+++ lambdadata_John_T/Operations.py
@@ -2,55 +2,57 @@
 and Division of two numbers"""
 
 
-class calculator:
-    # Program make a simple calculator
+class calculator(float):
+    def __init__(self, num1=None, num2=None):
+        super().__init__()
+        self.num2 = num2
+        self.num1 = num1
+
     # This function adds two numbers
-    def __init__(self):
-        # Program make a simple calculator
-        # This function adds two numbers
-        def add(x, y):
-            return x + y
+    def add(self, y):
+        return self + y
 
-        # This function subtracts two numbers
-        def subtract(x, y):
-            return x - y
+    # This function subtracts two numbers
+    def subtract(self, y):
+        return self - y
 
-        # This function multiplies two numbers
-        def multiply(x, y):
-            return x * y
+    # This function multiplies two numbers
+    def multiply(self, y):
+        return self * y
 
-        # This function divides two numbers
-        def divide(x, y):
-            return x / y
+    # This function divides two numbers
+    def divide(self, y):
+        return self / y
 
-        print("Select operation.")
-        print("1.Add")
-        print("2.Subtract")
-        print("3.Multiply")
-        print("4.Divide")
-        while True:
-            # Take input from the user
-            choice = input("Enter choice(1/2/3/4): ")
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
 
-            # Check if choice is one of the four options
-            if choice in ('1', '2', '3', '4'):
-                num1 = float(input("Enter first number: "))
-                num2 = float(input("Enter second number: "))
+    while True:
+        # Take input from the user
+        choice = input("Enter choice(1/2/3/4): ")
 
-                if choice == '1':
-                    print(num1, "+", num2, "=", add(num1, num2))
+        # Check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-                elif choice == '2':
-                    print(num1, "-", num2, "=", subtract(num1, num2))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1))
 
-                elif choice == '3':
-                    print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1))
 
-                elif choice == '4':
-                    print(num1, "/", num2, "=", divide(num1, num2))
-                break
-            else:
-                print("Invalid Input")
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1))
+
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1))
+            break
+        else:
+            print("Invalid Input")
 
 
 """
