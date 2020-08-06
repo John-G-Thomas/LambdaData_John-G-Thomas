21f2ff13ff24df89266c7d3a2c1559d1dfebe29d
John-G-Thomas <johnthomasg0215@gmail.com>
2020-08-06 17:23:07 -0400

    lecture notes

--- lambdadata_John_T/Operations.py
+++ lambdadata_John_T/Operations.py
@@ -2,27 +2,35 @@
 and Division of two numbers"""
 
 
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
 class calculator(float):
-    def __init__(self, num1=None, num2=None):
+    def __init__(self, x=None, y=None):
         super().__init__()
-        self.num2 = num2
-        self.num1 = num1
+        self.y = y
+        self.x = x
 
     # This function adds two numbers
-    def add(self, y):
-        return self + y
 
     # This function subtracts two numbers
-    def subtract(self, y):
-        return self - y
 
     # This function multiplies two numbers
-    def multiply(self, y):
-        return self * y
 
     # This function divides two numbers
-    def divide(self, y):
-        return self / y
 
     print("Select operation.")
     print("1.Add")
@@ -40,16 +48,16 @@ class calculator(float):
             num2 = float(input("Enter second number: "))
 
             if choice == '1':
-                print(num1, "+", num2, "=", add(num1))
+                print(num1, "+", num2, "=", add(num1, num2))
 
             elif choice == '2':
-                print(num1, "-", num2, "=", subtract(num1))
+                print(num1, "-", num2, "=", subtract(num1, num2))
 
             elif choice == '3':
-                print(num1, "*", num2, "=", multiply(num1))
+                print(num1, "*", num2, "=", multiply(num1, num2))
 
             elif choice == '4':
-                print(num1, "/", num2, "=", divide(num1))
+                print(num1, "/", num2, "=", divide(num1, num2))
             break
         else:
             print("Invalid Input")
@@ -77,3 +85,55 @@ class Math:
             return print(num1, ch, num2, ":", result)
         else:
             return print("Input character is not recognized!") """
+
+"""class calculator(float):
+    def __init__(self, x=None, y=None):
+        super().__init__()
+        self.y = y
+        self.x = x
+
+    # This function adds two numbers
+    def add(self, x, y):
+        return x + y
+
+    # This function subtracts two numbers
+    def subtract(self, x, y):
+        return x - y
+
+    # This function multiplies two numbers
+    def multiply(self, x, y):
+        return x * y
+
+    # This function divides two numbers
+    def divide(self, x, y):
+        return x / y
+
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+
+    while True:
+        # Take input from the user
+        choice = input("Enter choice(1/2/3/4): ")
+
+        # Check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
+
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+            break
+        else:
+            print("Invalid Input")"""
